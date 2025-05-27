0c1c9d22a2f43d9ea5cf61b4d8e46055be5327b5
nyanyacyan <nyanyacyan@gmail.com>
2025-05-27 15:23:36 +0900

    no message

--- installer/src/method/flow.py
+++ installer/src/method/flow.py
@@ -21,6 +21,15 @@ from method.base.utils.popup import Popup
 from method.base.selenium.click_element import ClickElement
 from method.base.utils.fileWrite import FileWrite
 
+# flow
+from method.flow_asia_travel_first import FlowAsiaTravelFirst
+from method.flow_asia_travel_second import FlowAsiaTravelSecond
+from method.flow_ja_automakers import FlowJaAutomakers
+from method.flow_trump import FlowTrump
+from method.flow_china_ev import FlowChinaEV
+from method.flow_korea import FlowKorea
+from method.flow_asia_train import FlowAsiaTrain
+from method.flow_ja_asia_entry import FlowJaAsiaEntry
 
 # const
 from method.const_element import ( CsvInfo, LoginInfo, ErrCommentInfo, PopUpComment, Element, )
@@ -63,14 +72,22 @@ class SingleProcess:
         self.click_element = ClickElement(chrome=self.chrome)
         self.file_write = FileWrite()
 
+        # flow
+        self.flow_asia_travel_first = FlowAsiaTravelFirst(chrome=self.chrome)
+        self.flow_asia_travel_second = FlowAsiaTravelSecond(chrome=self.chrome)
+        self.flow_ja_automakers = FlowJaAutomakers(chrome=self.chrome)
+        self.flow_trump = FlowTrump(chrome=self.chrome)
+        self.flow_china_ev = FlowChinaEV(chrome=self.chrome)
+        self.flow_korea = FlowKorea(chrome=self.chrome)
+        self.flow_asia_train = FlowAsiaTrain(chrome=self.chrome)
+        self.flow_ja_asia_entry = FlowJaAsiaEntry(chrome=self.chrome)
+
     # **********************************************************************************
     # ----------------------------------------------------------------------------------
 
-    def _single_process(self, url: str, keyword: str):
+    def _single_process(self):
         """各プロセスを実行する"""
         try:
-            self.logger.debug(f"url: {url}")
-
             # 引数にキーワード、期間、
             # ログイン
             self.login.flowLoginID(id_text=self.const_login_info['ID_INPUT_TEXT'], pass_text=self.const_login_info['PASS_INPUT_TEXT'], login_info=self.const_login_info)
@@ -90,81 +107,22 @@ class SingleProcess:
             else:
                 self.logger.info(f"{self.__class__.__name__} ログイン移行画面は表示されませんでした。")
 
-            # 新しいページでurlを開く
-            # self.get_element._open_new_page(url=url)
-            # self.random_sleep._random_sleep(2, 5)
 
-            # 詳細検索をクリック
-            self.click_element.clickElement(value=self.const_element['DETAIL_SEARCH_VALUE'])
-            self.random_sleep._random_sleep(2, 5)
+            self.flow_asia_travel_first.single_process()
 
-            # キーワードの入力
-            self.get_element.clickClearInput(value=self.const_element['KEYWORD_VALUE'], inputText=keyword)
-            self.random_sleep._random_sleep(2, 5)
+            self.flow_asia_travel_second.single_process()
 
-            # TODO 期間をクリック
-            self.click_element.clickElement(value=self.const_element['DETAIL_SEARCH_VALUE'])
-            self.random_sleep._random_sleep(2, 5)
+            self.flow_ja_automakers.single_process()
 
-            # TODO 1日以内をクリック
-            self.click_element.clickElement(value=self.const_element['DETAIL_SEARCH_VALUE'])
-            self.random_sleep._random_sleep(2, 5)
+            self.flow_trump.single_process()
 
-            # TODO 国をクリック
-            self.click_element.clickElement(value=self.const_element['DETAIL_SEARCH_VALUE'])
-            self.random_sleep._random_sleep(2, 5)
+            self.flow_china_ev.single_process()
 
-            # TODO 国を選択するためにクリック
+            self.flow_korea.single_process()
 
+            self.flow_asia_train.single_process()
 
-            # 要素のリスト取得（テーブルの取得）
-            main_element = self.get_element.getElement(by=self.const_element['BY_MAIN'], value=self.const_element['VALUE_MAIN'])
-            self.logger.info(f"main_element: {main_element}")
-
-            ul_elements = self.get_element.filterElement(parentElement=main_element, by=self.const_element['by_0'], value=self.const_element['value_0'])
-            self.logger.info(f"ul_elements: {ul_elements}")
-
-            li_elements = self.get_element.filterElements(parentElement=ul_elements, by=self.const_element['by_1'], value=self.const_element['value_1'])
-            self.logger.info(f"li_elements: {li_elements} 要素数: {len(li_elements)}つ")
-
-            csv_list = []
-            # 要素１つずつにアクセス
-            for e in li_elements:
-                self.logger.info(f"element: {e}")
-                # country_name_elements = self.get_element.filterElements(parentElement=e, value=self.const_element['value_2'])
-                # self.logger.info(f"country_name_elements: {country_name_elements} 要素数: {len(country_name_elements)}つ")
-
-                # 国名が書かれている要素を取得
-                country_name_element = self.get_element.filterElement(parentElement=e, value=self.const_element['value_2'])
-
-                # 国名,抽出
-                if country_name_element is None:
-                    self.logger.error(f"{self.__class__.__name__} 国名の要素が見つかりませんでした。")
-                    continue
-
-                country_name = country_name_element.text
-                self.logger.info(f"国名: {country_name}")
-
-                # URLが書かれている要素を取得
-                url_elements = self.get_element.filterElements(parentElement=e, by=self.const_element['by_4'], value=self.const_element['value_4'])
-                self.logger.info(f"url_elements: {url_elements} 要素数: {len(url_elements)}つ")
-
-                url_str = url_elements[1].get_attribute("href")
-                self.logger.info(f"url: {url_str}")
-
-                title_str = url_elements[1].text
-                self.logger.info(f"記事名: {title_str}")
-
-                # リストに追加
-                csv_list.append([country_name, title_str, url_str])
-
-            self.logger.info(f"csv_list: {csv_list}")
-
-            # csvファイルに書き込めるように修正
-            file_name = f"nna_{self.timestamp_two}"
-
-            # csvファイルに書き込み
-            self.file_write.write_cst_to_list(col_names=self.const_csv_info['COL_NAME'], data=csv_list, fileName=file_name)
+            self.flow_ja_asia_entry.single_process()
 
 
         except TimeoutError:
